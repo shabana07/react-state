f46b7542c45e6a6e498c6afa6625811a7472be21
sohakhan77 <sohakhank73@gmail.com>
2021-10-16 23:01:45 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = [{
+      img: "./images/bag-1.jpg",
+      title: "Hand Bag",
+      price: "Rs-2500"
+    },
+    {
+      img: "./images/shoes-1.jpg",
+      title: "Footwear",
+      price: "Rs-1000"
+    },
+    {
+      img: "./images/glasses-1.jpg",
+      title: "Glasses",
+      price: "Rs-700"
+    },
+    {
+      img: "./images/scarve-1.jpg",
+      title: "Scarf",
+      price: "Rs-500"
+    },
+    {
+      img: "./images/bag-1.jpg",
+      title: "bag",
+      price: "Rs-2500"
+    },
+    {
+      img: "./images/jewell-1.jpg",
+      title: "Jewellry",
+      price: "Rs-2500"
+    }
+    ]
+  }
+ render() {
+    return (
+      <>
+      <div className="main">
+      {
+          this.state.map((a) => {
+            return (
+              <>
+                  <div className="card my-2" style={{ width: '10rem', height: '15rem' }}>
+                    <img src={a.img} className="card-img-top" alt="..." style={{ width: '10rem', height: '10rem' }} />
+                    <div className="card-body">
+                      <h5 className="card-title">{a.title}</h5>
+                      <p className="card-text">{a.price}</p>
+                    </div>
+                  </div>
+              </>
+            )
+          })
+        }
+        </div> 
+      </>
+    )
+  }
 }
-
 export default App;
